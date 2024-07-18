007d5e3daadff60b2fd1511f45a79db81e6acbd9
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2024-07-18 12:11:45 +0000

    嗨嗨嗨

--- uav1/transmit.sh
+++ uav1/transmit.sh
@@ -1,6 +1,6 @@
 cd scripts/datasave/
 zip -r uav1.zip uav1
-sudo scp -P 22 uav1.zip allen@192.168.10.54:/home/allen/yangyefeng/datasave
+sudo scp -P 22 uav1.zip yefeng@192.168.50.172:/home/yefeng/yefengGithub/uav_consensus_rl_ros/src/uav_consensus_rl_ros/master/datasave
 
 
 # cd other/

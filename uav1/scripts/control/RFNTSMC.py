f60a8dc1d6a5744cf90ab8f242541c2a048ba1f8
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2024-07-23 09:01:01 +0000

    fixed some bugs

--- uav1/scripts/control/RFNTSMC.py
+++ uav1/scripts/control/RFNTSMC.py
@@ -174,6 +174,43 @@ class rfntsmc_consensus(rfntsmc):
                                        Lambda_eta: np.ndarray,
                                        ref: np.ndarray,
                                        d_ref: np.ndarray,
+                                       dd_ref: np.ndarray,
+                                       nu: np.ndarray,
+                                       d_nu: np.ndarray,
+                                       dd_nu: np.ndarray,
+                                       e_max: float = 0.2,
+                                       dot_e_max: float = 1.5):
+        if e_max is not None:  # 增加对位置误差的输入饱和
+            consensus_e = np.clip(consensus_e, -e_max, e_max)
+        if dot_e_max is not None:  # 增加对速度误差的输入饱和
+            consensus_de = np.clip(consensus_de, -dot_e_max, dot_e_max)
+
+        pos_com = b * self.k_com_pos * ref + self.k_com_pos * nu
+        vel_com = b * self.k_com_vel * d_ref + self.k_com_vel * d_nu
+        acc_com = b * self.k_com_acc * dd_ref + self.k_com_acc * dd_nu
+
+        self.sigma_o = (consensus_de + vel_com) + self.k1 * (consensus_e + pos_com) + self.gamma * self.sig((consensus_e + pos_com), self.alpha)
+        self.dot_sigma_o1 = self.sig(self.sigma_o, self.beta)
+        self.sigma_o1 += self.dot_sigma_o1 * self.dt
+        self.so = self.sigma_o + self.lmd * self.sigma_o1
+        
+        u1 = (Lambda_eta + self.k1 * (consensus_de + vel_com) +
+              self.gamma * self.alpha * np.fabs((consensus_e + pos_com)) ** (self.alpha - 1) * (consensus_de + vel_com) +
+              self.lmd * self.dot_sigma_o1)
+        u2 = self.k2 * self.so
+        
+        self.yyf_i += self.k_yyf_i * consensus_e
+        self.yyf_p = self.k_yyf_p * consensus_e
+        self.yyf_d = self.k_yyf_d * consensus_de
+        
+        self.control_out_consensus = -(u1 + u2 + self.yyf_i + self.yyf_p + self.yyf_d + acc_com) / (d + b)
+                                       b: float,
+                                       d: float,
+                                       consensus_e: np.ndarray,
+                                       consensus_de: np.ndarray,
+                                       Lambda_eta: np.ndarray,
+                                       ref: np.ndarray,
+                                       d_ref: np.ndarray,
                                        e_max: float = 0.2,
                                        dot_e_max: float = 1.5):
         if e_max is not None:  # 增加对位置误差的输入饱和

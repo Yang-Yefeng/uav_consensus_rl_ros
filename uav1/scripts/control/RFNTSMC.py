0d448c41f8500a0d903711d32f3f036e9251a43f
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2024-07-23 09:09:15 +0000

    fixed some bugs

--- uav1/scripts/control/RFNTSMC.py
+++ uav1/scripts/control/RFNTSMC.py
@@ -204,37 +204,3 @@ class rfntsmc_consensus(rfntsmc):
         self.yyf_d = self.k_yyf_d * consensus_de
         
         self.control_out_consensus = -(u1 + u2 + self.yyf_i + self.yyf_p + self.yyf_d + acc_com) / (d + b)
-                                       b: float,
-                                       d: float,
-                                       consensus_e: np.ndarray,
-                                       consensus_de: np.ndarray,
-                                       Lambda_eta: np.ndarray,
-                                       ref: np.ndarray,
-                                       d_ref: np.ndarray,
-                                       e_max: float = 0.2,
-                                       dot_e_max: float = 1.5):
-        if e_max is not None:  # 增加对位置误差的输入饱和
-            consensus_e = np.clip(consensus_e, -e_max, e_max)
-        if dot_e_max is not None:  # 增加对速度误差的输入饱和
-            consensus_de = np.clip(consensus_de, -dot_e_max, dot_e_max)
-        
-        # s = (consensus_de + self.k_com_vel * d_ref) + self.k1 * (consensus_e + self.k_com_pos * ref) + self.k2 * self.sig(consensus_e + self.k_com_pos * ref, self.alpha1)
-        # sigma = (self.k1 + self.k2 * self.alpha1 * self.sig(consensus_e + self.k_com_pos * ref, self.alpha1 - 1)) * (consensus_de + self.k_com_vel * d_ref)
-        # u1 = Lambda_eta + sigma
-        # u2 = self.k3 * np.tanh(5 * s) + self.k4 * self.sig(s, self.alpha2)
-        
-        self.sigma_o = (consensus_de + self.k_com_vel * d_ref) + self.k1 * (consensus_e + self.k_com_pos * ref) + self.gamma * self.sig((consensus_e + self.k_com_pos * ref), self.alpha)
-        self.dot_sigma_o1 = self.sig(self.sigma_o, self.beta)
-        self.sigma_o1 += self.dot_sigma_o1 * self.dt
-        self.so = self.sigma_o + self.lmd * self.sigma_o1
-        
-        u1 = (Lambda_eta + self.k1 * (consensus_de + self.k_com_vel * d_ref) +
-              self.gamma * self.alpha * np.fabs((consensus_e + self.k_com_pos * ref)) ** (self.alpha - 1) * (consensus_de + self.k_com_vel * d_ref) +
-              self.lmd * self.dot_sigma_o1)
-        u2 = self.k2 * self.so
-        
-        self.yyf_i += self.k_yyf_i * consensus_e
-        self.yyf_p = self.k_yyf_p * consensus_e
-        self.yyf_d = self.k_yyf_d * consensus_de
-        
-        self.control_out_consensus = -(u1 + u2 + self.yyf_i + self.yyf_p + self.yyf_d) / (d + b)

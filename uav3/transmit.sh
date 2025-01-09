067f9617816fff477a5b80a16c2eaeb34c53d51f
YANG-Yefeng <21037385r@connect.polyu.hk>
2025-01-09 13:42:14 +0800

    only for uav0

--- uav3/transmit.sh
+++
@@ -1,7 +0,0 @@
-cd scripts/datasave/
-zip -r uav3.zip uav3
-sudo scp -P 22 uav3.zip yefeng@192.168.50.172:/home/yefeng/yefengGithub/uav_consensus_rl_ros/src/uav_consensus_rl_ros/master/datasave
-
-
-# cd other/
-# sudo scp -P 22 data_voltage_thrust.csv allen@192.168.10.54:/home/allen/yangyefeng/datasave

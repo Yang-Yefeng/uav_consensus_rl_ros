bcac6a6a2bc5810830db8b6fa2e96fb4fe920ac1
YANG-Yefeng <21037385r@connect.polyu.hk>
2025-01-09 14:08:14 +0800

    for uav1 only

--- uav2/transmit.sh
+++
@@ -1,7 +0,0 @@
-cd scripts/datasave/
-zip -r uav2.zip uav2
-sudo scp -P 22 uav2.zip yefeng@192.168.50.172:/home/yefeng/yefengGithub/uav_consensus_rl_ros/src/uav_consensus_rl_ros/master/datasave
-
-
-# cd other/
-# sudo scp -P 22 data_voltage_thrust.csv allen@192.168.10.54:/home/allen/yangyefeng/datasave

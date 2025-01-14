a32eb8d1eaaaa3f79a8e3fb1ac95eeff1e8142bd
yefengpolyu <yefeng.yang@connect.polyu.hk>
2025-01-14 13:26:17 +0800

    debug

--- uav0/transmit.sh
+++ uav0/transmit.sh
@@ -1,6 +1,6 @@
 cd scripts/datasave/
 zip -r uav0.zip uav0
-sudo scp -P 22 uav0.zip allen@192.168.10.54:/home/allen/yangyefeng/datasave
+sudo scp -P 22 uav0.zip yefeng@192.168.50.172:/home/yefeng/yefengGithub/uav_consensus_rl_ros/src/uav_consensus_rl_ros/master/datasave
 
 
 # cd other/
